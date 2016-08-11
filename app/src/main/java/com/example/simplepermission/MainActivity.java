863dd880a1b9d93718e8cf12ecc4d9e1e25ceedf
ghalibse <ghalibse@gmail.com>
2016-08-11 17:45:31 -0400

    After SDK @23

--- app/src/main/java/com/example/simplepermission/MainActivity.java
+++ app/src/main/java/com/example/simplepermission/MainActivity.java
@@ -1,21 +1,31 @@
 package com.example.simplepermission;
 
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivityTAG_";
+
+    private static final int CODE_CONSTANT = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +34,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void doMagic(View view) {
-        writeTimeExternal();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "doMagic: " + "We don't have permission");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, CODE_CONSTANT);
+        } else {
+            Log.d(TAG, "doMagic: " + "We already have permission");
+            writeTimeExternal();
+        }
     }
 
-    public void writeTimeExternal() {
-        File file = android.os.Environment.getExternalStorageDirectory();
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
-        File aux = new File(file, "hello.txt");
+        switch (requestCode) {
+            case CODE_CONSTANT:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "onRequestPermissionsResult: " + "User gave permission");
+                    writeTimeExternal();
+                } else {
+                    Log.d(TAG, "onRequestPermissionsResult: " + "User didn't give permission");
+                }
+        }
+
+    }
+
+    private void writeTimeExternal() {
+        File file = android.os.Environment.getExternalStorageDirectory();
+        File aux = new File(file, "hello24.txt");
 
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(aux);
             PrintWriter printWriter = new PrintWriter(fileOutputStream);
-            printWriter.println(System.currentTimeMillis());
+            printWriter.println(System.currentTimeMillis()); //Writing current milliseconds
             printWriter.flush();
             printWriter.close();
             fileOutputStream.close();
@@ -53,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         readFileExternal();
     }
 
-    public void readFileExternal() {
+    private void readFileExternal() {
         File file = android.os.Environment.getExternalStorageDirectory();
         File aux = new File(file, "hello.txt");
 
