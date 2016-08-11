cd53750af85afa86cee865fa5a9af9dc03189dcd
ghalibse <ghalibse@gmail.com>
2016-08-11 17:15:35 -0400

    Before SDK 22

--- app/src/main/java/com/example/simplepermission/MainActivity.java
+++ app/src/main/java/com/example/simplepermission/MainActivity.java
@@ -2,9 +2,20 @@ package com.example.simplepermission;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +24,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void doMagic(View view) {
+        writeTimeExternal();
+    }
+
+    public void writeTimeExternal() {
+        File file = android.os.Environment.getExternalStorageDirectory();
+
+        File aux = new File(file, "hello.txt");
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(aux);
+            PrintWriter printWriter = new PrintWriter(fileOutputStream);
+            printWriter.println(System.currentTimeMillis());
+            printWriter.flush();
+            printWriter.close();
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "doMagic: " + file);
+        Log.d(TAG, "doMagic: " + aux);
     }
 
     public void readMagic(View view) {
+        readFileExternal();
+    }
+
+    public void readFileExternal() {
+        File file = android.os.Environment.getExternalStorageDirectory();
+        File aux = new File(file, "hello.txt");
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(aux);
+            Scanner scanner = new Scanner(fileInputStream);
+            Log.d(TAG, "readMagic: " + scanner.next());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
